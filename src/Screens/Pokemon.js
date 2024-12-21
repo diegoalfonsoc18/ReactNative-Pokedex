b77cc8a68322c695cdaf7bab7f7b6d260c69a02c
diegoalfonsoc18 <dlalfonso57@gmail.com>
2024-12-20 20:59:08 -0500

    Add Pokemon screen and update navigation

---
+++ src/Screens/Pokemon.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Pokemon() {
+  return (
+    <View>
+      <Text>Esto es un Pokemon</Text>
+    </View>
+  );
+}
--- src/navigation/PokedexNavigation.js
+++ src/navigation/PokedexNavigation.js
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import PokedexScreen from "../Screens/Pokedex";
+import PokemonScreen from "../Screens/Pokemon";
 
 const Stack = createNativeStackNavigator();
 export default function PokedexNavigation() {
@@ -9,9 +10,12 @@ export default function PokedexNavigation() {
       <Stack.Screen
         name="Pokedex"
         component={PokedexScreen}
-        options={{
-          title: "Pokedex",
-        }}
+        options={{ title: "Pokedex" }} // Añadido título para la pantalla Pokedex
+      />
+      <Stack.Screen
+        name="Pokemon"
+        component={PokemonScreen}
+        options={{ title: "Pokemon" }} // Añadido título para la pantalla Pokemon
       />
     </Stack.Navigator>
   );
