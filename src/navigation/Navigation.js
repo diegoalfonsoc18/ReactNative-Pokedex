68dd297166eadd9b0375dbd3b6f3b3ab9cd21e00
diegoalfonsoc18 <dlalfonso57@gmail.com>
2024-12-20 20:09:24 -0500

    menu navigation

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import { Image } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Favorites from "../Screens/Favorites";
 import Pokedex from "../Screens/Pokedex";
@@ -9,9 +11,43 @@ const Tab = createBottomTabNavigator();
 export default function Navigation() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Favorites" component={Favorites} />
-      <Tab.Screen name="Pokedex" component={Pokedex} />
-      <Tab.Screen name="Account" component={Account} />
+      <Tab.Screen
+        name="Favorites"
+        component={Favorites}
+        options={{
+          tabBarLabel: "Favoritos",
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="heart" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Pokedex"
+        component={Pokedex}
+        options={{
+          tabBarLabel: "",
+          tabBarIcon: () => renderPokeball(),
+        }}
+      />
+      <Tab.Screen
+        name="Account"
+        component={Account}
+        options={{
+          tabBarLabel: "Mi Cuenta",
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="user" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
+  );
+}
+
+function renderPokeball() {
+  return (
+    <Image
+      source={require("../assets/icon-pokebola.png")}
+      style={{ width: 75, height: 75, top: -18 }}
+    />
   );
 }
