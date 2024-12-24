4868561edd2ca0a489e38c7b5db85f7ebaa27211
diegoalfonsoc18 <dlalfonso57@gmail.com>
2024-12-24 12:36:38 -0500

    StackNavigation fixed

--- src/navigation/PokedexNavigation.js
+++ src/navigation/PokedexNavigation.js
@@ -8,14 +8,14 @@ export default function PokedexNavigation() {
   return (
     <Stack.Navigator>
       <Stack.Screen
+        name="Pokemon"
+        component={PokemonScreen}
+        options={{ title: "Pokemon", headerShown: false }} // Añadido título para la pantalla Pokemon
+      />
+      <Stack.Screen
         name="Pokedex"
         component={PokedexScreen}
         options={{ title: "Pokedex", headerShown: false }} // Añadido título para la pantalla Pokedex
-      />
-      <Stack.Screen
-        name="Pokemon"
-        component={PokemonScreen}
-        options={{ title: "Pokemon" }} // Añadido título para la pantalla Pokemon
       />
     </Stack.Navigator>
   );
