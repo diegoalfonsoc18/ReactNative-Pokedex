95ebefb7d36b8595af01bd37b3fdb4995c5e3538
diegoalfonsoc18 <dlalfonso57@gmail.com>
2024-12-24 13:23:52 -0500

    configuration API

--- src/Screens/Pokedex.js
+++ src/Screens/Pokedex.js
@@ -1,12 +1,41 @@
 import { Text, SafeAreaView } from "react-native";
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import { getPokemonsApi, getPokemonDetailsByUrlApi } from "../api/pokemon";
 
-export default class Pokedex extends Component {
-  render() {
-    return (
-      <SafeAreaView>
-        <Text>Pokedex</Text>
-      </SafeAreaView>
-    );
-  }
+export default function Pokedex() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(async () => {
+    (async () => {
+      await loadPokemons();
+    })();
+  }, []);
+
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemonsApi();
+
+      const pokemonsArray = [];
+      for await (const pokemon of response.results) {
+        const pokemonDetail = await getPokemonDetailsByUrlApi(pokemon.url);
+        pokemonsArray.push({
+          id: pokemonDetail.id,
+          name: pokemonDetail.name,
+          types: pokemonDetail.types[0].type.name,
+          order: pokemonDetail.order,
+          imagen: pokemonDetail.sprites.other["official-artwork"].front_default,
+          url: pokemon.url,
+        });
+      }
+      setPokemons([...pokemons, ...pokemonsArray]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <SafeAreaView>
+      <Text>Pokedex</Text>
+    </SafeAreaView>
+  );
 }
